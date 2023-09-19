8584ec6573515b6a63beb03995a1cf089976be0f
ikrasilnikov <ikrasilnikov@webim-team.ru>
2023-09-19 10:29:12 +0400

    Изучение работы запросов. Добавил запрос для "+лампочка".

--- flashing_lights/main.py
+++ flashing_lights/main.py
@@ -8,10 +8,26 @@ async def index_page(request):
     return {'title': 'Flashing Lights', 'pulls': 3, 'lights': 4}
 
 
-if __name__ == '__main__':
+async def plus_light(request):
+    data = await request.json()
+    print(data)
+    return web.json_response('ok')
+
+
+async def init_app():
     app = web.Application()
     aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('templates'))
     app.router.add_static('/static', 'static', name='static')
     app.router.add_get('/', index_page)
+    app.router.add_post('/post', plus_light)
 
+    return app
+
+
+def main():
+    app = init_app()
     web.run_app(app)
+
+
+if __name__ == '__main__':
+    main()
--- flashing_lights/static/script.js
+++ flashing_lights/static/script.js
@@ -1,6 +1,5 @@
 function hide(item) {
     console.log('hide');
-    console.log(item.value);
     item.classList.toggle('hidden');
 }
 function changeColor(item) {
@@ -27,8 +26,31 @@ let close_btns = document.querySelectorAll('#settings .pull .close');
 let color_btns = document.querySelectorAll('#settings .pull .color');
 
 plus_btns.forEach(function (item, i, arr) {
-    item.onclick = () => hide(item);
+    item.onclick = function() {
+        console.log(item);
+        hide(item);
+        post_function(item);
+    };
 });
 color_btns.forEach(function (item, i, arr) {
     item.onclick = () => changeColor(item);
 });
+
+
+function post_function(item) {
+    let post_item = {
+        'id': item.parentElement.id,
+        'color': item.parentElement.children[0].innerHTML,
+        'value': item.parentElement.children[1].value
+    }
+
+    fetch('http://0.0.0.0:8080/post', {
+        method: 'POST',
+        headers: {
+           "Content-type": "application/json; charset=UTF-8"
+        },
+        body: JSON.stringify(post_item)
+    })
+        .then(response => response.json())
+        .then(json => console.log(JSON.stringify(json)));
+}
