e64591cd39f107cb2639cfbc31fcff404f9c27aa
ikrasilnikov <ikrasilnikov@webim-team.ru>
2023-09-19 19:36:33 +0400

    Доработал появление "фонариков". Пытаюсь понять как запустить event_loop.

--- flashing_lights/main.py
+++ flashing_lights/main.py
@@ -1,31 +1,44 @@
 from aiohttp import web
 import jinja2
 import aiohttp_jinja2
+import asyncio
+
+tasks = []
 
 
 @aiohttp_jinja2.template('index.jinja2')
 async def index_page(request):
-    return {'title': 'Flashing Lights', 'pulls': 3, 'lights': 4}
+    pulls = 3
+    lights = 2
+    return {'title': 'Flashing Lights', 'pulls': pulls, 'lights': lights}
 
 
-async def plus_light(request):
+async def add_light(request):
     data = await request.json()
     print(data)
     return web.json_response('ok')
 
 
+async def add_setting(request):
+    return web.json_response('ok')
+
+
 async def init_app():
     app = web.Application()
+
     aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('templates'))
+
     app.router.add_static('/static', 'static', name='static')
     app.router.add_get('/', index_page)
-    app.router.add_post('/post', plus_light)
+    app.router.add_post('/add_light', add_light)
+    app.router.add_post('/add_setting', add_setting)
 
     return app
 
 
 def main():
-    app = init_app()
+    loop = asyncio.get_event_loop()
+    app = loop.run_until_complete(init_app())
     web.run_app(app)
 
 
--- flashing_lights/static/script.js
+++ flashing_lights/static/script.js
@@ -1,6 +1,7 @@
-function hide(item) {
-    console.log('hide');
-    item.classList.toggle('hidden');
+function switch_visual(items) {
+    items.forEach(function (item, i, arr) {
+        item.classList.toggle('d-none');
+    })
 }
 function changeColor(item) {
     if (item.classList.contains('btn-primary')) {
@@ -19,38 +20,52 @@ function changeColor(item) {
         item.innerHTML = 'Blue'
     }
 }
+function addLight(item) {
+    let post_item = {
+        'id': item.parentElement.id.split('-')[1],
+        'color': item.parentElement.children[0].innerHTML,
+        'value': item.parentElement.children[1].value
+    }
+
+    fetch('http://0.0.0.0:8080/add_light', {
+        method: 'POST',
+        body: JSON.stringify(post_item)
+    })
+        .then(response => response.json())
+        .then(json => console.log(JSON.stringify(json)));
+}
+function addSetting() {
+    fetch('http://0.0.0.0:8080/add_setting', {
+        method: 'POST',
+    })
+        .then(response => response.json())
+        .then(json => console.log(JSON.stringify(json)));
+}
+function flash(item) {
+    item.classList.toggle('active')
+    setTimeout(() => item.classList.toggle('active'), 200);
+}
+
 
 let plus_btns = document.querySelectorAll('#settings .pull .plus');
 let reload_btns = document.querySelectorAll('#settings .pull .reload');
 let close_btns = document.querySelectorAll('#settings .pull .close');
 let color_btns = document.querySelectorAll('#settings .pull .color');
 
+let lights = document.querySelectorAll('#lights .light')
+
 plus_btns.forEach(function (item, i, arr) {
     item.onclick = function() {
         console.log(item);
-        hide(item);
-        post_function(item);
+        switch_visual(item.parentElement.querySelectorAll('.plus, .reload, .close'));
+        addLight(item);
+        addSetting();
     };
 });
 color_btns.forEach(function (item, i, arr) {
     item.onclick = () => changeColor(item);
 });
 
-
-function post_function(item) {
-    let post_item = {
-        'id': item.parentElement.id,
-        'color': item.parentElement.children[0].innerHTML,
-        'value': item.parentElement.children[1].value
-    }
-
-    fetch('http://0.0.0.0:8080/post', {
-        method: 'POST',
-        headers: {
-           "Content-type": "application/json; charset=UTF-8"
-        },
-        body: JSON.stringify(post_item)
-    })
-        .then(response => response.json())
-        .then(json => console.log(JSON.stringify(json)));
-}
+lights.forEach(function (item, i, arr) {
+    item.onclick = () => flash(item);
+});
